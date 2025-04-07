2b7d72bd82dd7f722b338d1ffa3d84ff846709e6
Tacash444 <328234836@idf.il>
2025-04-07 11:51:18 +0300

    rename(ConstraintTypeEnum.py) renamed IT

--- 1 - YAML LZ Configuration/OrgPolicyConstraints/BooleanConstraint.py
+++ 1 - YAML LZ Configuration/OrgPolicyConstraints/BooleanConstraint.py
@@ -1,5 +1,6 @@
 from Constraint import Constraint
 from ConstraintType import ConstraintType
+
 class BooleanConstraint(Constraint):
     def __init__(self, name):
         super().__init__(name, ConstraintType.Boolean)
---
+++ 1 - YAML LZ Configuration/OrgPolicyConstraints/ConstraintTypeEnum.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class ConstraintType(Enum):
+    Boolean = "Boolean"
+    List = "List"
